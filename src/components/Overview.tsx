f139eae34e133d960c638be16e725f7f72f2420c
Ebru Kaya <ebru@ebrukaya.me>
2023-06-11 08:16:23 -0500

    base overview component added

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,12 +1,16 @@
+import { ArrowPathRoundedSquareIcon, BuildingOfficeIcon } from '@heroicons/react/24/solid'
+
 const Overview = () => {
   return (
     <div className='w-full'>
       <div className='flex flex-col sm:flex-row gap-x-10 gap-y-4 sm:gap-y-0'>
-        <div>
-          Cleaning
+        <div className='flex items-center'>
+          <ArrowPathRoundedSquareIcon className='h-8 w-8 text-green-500 mr-2' />
+          <span>Cleaning: 20</span>
         </div>
-        <div>
-          Occupancy
+        <div className='flex items-center'>
+          <BuildingOfficeIcon className='h-8 w-8 text-blue-500 mr-2' />
+          <span>Occupancy: 80%</span>
         </div>
       </div>
     </div>
